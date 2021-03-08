e12470166a17dbdf706b7d81b3d262152e15f5c2
Raúl Marín <raul.marin.leal@gmail.com>
2021-03-08 09:47:24 +0100

    -bugfix on payment controller

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,10 +31,10 @@ module.exports = function(grunt) {
 
             },
             composerProd: {
-                command: 'composer install --prefer-source --no-dev'
+                command: 'composer install --no-dev'
             },
             composerDev: {
-                command: 'composer install --prefer-source --ignore-platform-reqs'
+                command: 'composer install --ignore-platform-reqs'
             },
             runTestPrestashop17: {
                 command:
